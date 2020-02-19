c555b51c6ffbf5d61e3bc6dad37ac8ce580d17b5
Emmus Monsalve <emonsalvep38@gmail.com>
2020-02-19 17:27:25 -0500

    add test to class user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""In this file we prove that the user file, that the class works correctly"""
+
+import unittest
+import pep8
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.state import State
+from models.review import Review
+
+
+class Testuser(unittest.TestCase):
+    """Class with the methods to prove that the user class works correctly"""
+    def test_pep8_conformance_user(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/user.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
